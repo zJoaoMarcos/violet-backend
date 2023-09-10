ee21b2230de770c4c7887d7fe03ae217a2c31a53
João Matos <joaomarcosm.silva@outlook.com>
2023-09-10 10:27:28 -0300

    create route to delete plant

--- src/infra/http/controllers/plants/routes.ts
+++ src/infra/http/controllers/plants/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 
 import { verifyJwt } from "../../middlewares/verify-jwt";
+import { deletePlant } from "./delete-plant";
 import { editPlant } from "./edit-plant";
 import { fetchAllUserPlants } from "./fetch-all-user-plants";
 import { findPlantById } from "./find-plant-by-id";
@@ -13,7 +14,5 @@ export async function plantsRoutes(app: FastifyInstance) {
   app.put(":plantId", editPlant);
   app.get("owner/:ownerId", fetchAllUserPlants);
   app.get(":plantId", findPlantById);
-  /*TODO: create this usecase and rote
-   app.delete(':plantID', deletePlant );
-   */
+  app.delete(":plantId/owner/:ownerId", deletePlant);
 }
